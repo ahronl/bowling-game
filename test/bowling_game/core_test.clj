b87908cbeb0a25359b88d3488dc3e6ce49abbbfc
ahronl <ahrolster@gmail.com>
2018-06-28 22:30:08 +0300

    gutter game test

--- test/bowling_game/core_test.clj
+++ test/bowling_game/core_test.clj
@@ -4,4 +4,8 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 0))))
+
+(deftest gutter-game-test
+    (testing "When calculating the score of a bowling game it returns a zero score for a complete gutter game"
+        (is (= (score (repeat 20 0)) 0))))
